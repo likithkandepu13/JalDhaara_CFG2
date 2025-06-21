bfff19bc9e87afcbf6a7e4802b1501daa14d65b1
likithkandepu13 <withlikith@gmail.com>
2025-06-21 19:35:26 +0530

    added json files

---
+++ frontend/jaladhaarapp/src/admin/adminNavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const adminNavBar = () => {
+  return (
+    <div>adminNavBar</div>
+  )
+}
+
+export default adminNavBar
---
+++ frontend/jaladhaarapp/src/donor/donarHome.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import admin from '../Data'
+
+export const donarHome = () => {
+  return (  
+    <div>donarHome</div>
+  )
+}
